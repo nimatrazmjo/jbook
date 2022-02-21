7fa0e27c9cd131d5b01535ca6231ccd3535a46c5
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-02-21 18:34:38 +0430

    bundle npm package

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
 
-function App() {
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+import * as esbuild from 'esbuild-wasm';
+
+const App: React.FC = () => {
+  const ref = useRef({})
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    await esbuild.initialize({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+    ref.current = true
+  }
+
+  useEffect(() => {
+    startService();
+  }, [ref])
+
+  const onClick = async () => {
+    if (!ref.current) return;
+
+    const result = await esbuild.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
+    });
+    console.log(result);
+    
+    setCode(result.outputFiles[0].text);
+
+
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-component">
+      <textarea name="code" id="code" value={input} onChange={e => setInput(e.target.value)} ></textarea>
+      <div className="button">
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>
+        {code}
+      </pre>
     </div>
-  );
+  )
+
 }
 
 export default App;
